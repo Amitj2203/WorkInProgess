3e34453bf6e3fd9a074cddde3e788238ae586a87
Amit Kumar Jha <amit.kumar.jha@schibsted.com>
2024-07-22 12:32:55 +0200

    From VsCode

---
+++ github-cicd-actions/src/main/java/com/learningisfun/github_cicd_actions/GithubCicdActionsApplication.java
@@ -0,0 +1,18 @@
+package com.learningisfun.github_cicd_actions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GithubCicdActionsApplication {
+    @GetMapping("/welcome")
+	public String welcome(){
+			return "Welcome to LearningIsFun";
+	}
+	 	public static void main(String[] args) {
+		SpringApplication.run(GithubCicdActionsApplication.class, args);
+	}
+}
